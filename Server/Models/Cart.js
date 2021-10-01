e6b5730f90087719e6583c60fd67c8bde433f7bc
BINGOO <54973413+Royalatomo@users.noreply.github.com>
2021-10-01 21:14:20 +0530

    Update Cart.js

--- Server/Models/Cart.js
+++ Server/Models/Cart.js
@@ -1,25 +1,35 @@
+// Importing Function
 const mongoose = require("mongoose");
+
+// Creating Mongoose Model
 const Cart = mongoose.model('Cart', {
+
     useremail: {
         type: String
     },
+
     items: [{
         productId: {
             type: String,
         },
+
         productName: {
             type: String
         },
+
         quantity: {
             type: Number,
         },
+
         price: {
             type: Number
         }
     }],
+
     totalPrice: {
         type: Number,
     }
 });
 
-module.exports = {Cart};
+// Exporting Model
+module.exports = { Cart };
