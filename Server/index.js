3fdefc37d8679fee2372b2fac7dacf6ee5642360
BINGOO <54973413+Royalatomo@users.noreply.github.com>
2021-10-01 18:19:08 +0530

    Update index.js
    Made it new db.js compatitable

--- Server/index.js
+++ Server/index.js
@@ -1,13 +1,15 @@
-const express=require("express");
-require("./db");
-const router=require("./Controller");
+const express = require("express");
+const runDB = require("./db");
+const router = require("./Controller");
 const cookieParser = require('cookie-parser');
-var app=express();
 
-const port=process.env.PORT || 5000;
+var app = express();
+runDB();
+
+const port = process.env.PORT || 5000;
 
 
-var allowCrossDomain = function(req, res, next) {
+var allowCrossDomain = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', "*");
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
@@ -22,5 +24,4 @@ app.use(cookieParser());
 
 app.use(router);
 
-app.listen(port,()=>console.log(`Listening at ${port}`));
-
+app.listen(port, () => console.log(`Listening at ${port}`));
